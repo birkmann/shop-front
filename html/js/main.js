7709a2d2523140de74f5d5cc374c9a3486a69527
Rafael Birkmann <mail@rafaelbirkmann.de>
2020-01-10 16:17:49 +0100

    files

--- html/js/main.js
+++ html/js/main.js
@@ -2,3 +2,7 @@ var el = document.querySelector('header.main .menu-toggle');
 el.onclick = function() {
 	document.querySelector('body').classList.toggle('show-menu');
 }
+
+document.querySelector('.close-side').onclick = function() {
+	document.querySelector('body').classList.toggle('show-menu');
+}
